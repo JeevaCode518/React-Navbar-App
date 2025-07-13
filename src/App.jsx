268c73e6a24d3683240bc17b34a2835873a1a96f
Jeeva <jeevarathinam518@gmail.com>
2025-07-13 12:26:59 +0530

    feat: Created a simple react nav bar application

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./Components/Navbar"
+import Footer from "./Components/Footer"
+import Table from "./Components/Table"
+
+function App() {
+  const items = ["A","B", "C", "D", "E", "F"];
+  return(
+    <>
+      <NavBar/>
+      <Table items={items} heading="Dynamic List"/>
+      <Footer/>
+    </>
+  
+  )
+}
+
+export default App
